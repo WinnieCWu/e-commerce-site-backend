cbfcab91c6465433a618eadd0b49028cdefa9ff2
WinnieCWu <winniechn7@gmail.com>
2022-04-16 10:23:01 -0700

    added Category model

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # e-commerce-site-backend
+##What this app does
+##What it looks like
+##Contributors
+Thankful for starter code through https://github.com/coding-boot-camp/fantastic-umbrella
---
+++ models/index.js
@@ -0,0 +1,31 @@
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+// Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'product_id'
+});
+
+// Categories have many Products
+Category.hasMany(Products, {
+  foreignKey: 'category_id'
+});
+
+// Products belongToMany Tags (through ProductTag)
+Product.belongToMany(Tag, {
+  foreignKey: 'productTag_id'
+});
+// Tags belongToMany Products (through ProductTag)
+Tag.belongToMany(Product, {
+  foreignKey:'productTag_id'
+});
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
