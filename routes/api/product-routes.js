96f7642b91fdc89ce6486bc09bb6985b1ca1d8a7
WinnieCWu <winniechn7@gmail.com>
2022-04-21 22:13:37 -0700

    checked for functionality

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -74,6 +74,7 @@ router.post('/', (req, res) => {
     product_name: req.body.product_name,
     price: req.body.price,
     stock: req.body.stock,
+    category_id: req.body.category_id,
   })
     .then(product => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
